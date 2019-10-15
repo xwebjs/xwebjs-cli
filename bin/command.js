d45d98335fed7d81a158be30c188ca79c80396c2
nick <nick@163.com>
2019-10-15 23:47:54 +0800

    Add framework modules

--- bin/command.js
+++ bin/command.js
@@ -1,15 +1,13 @@
-#!/usr/bin/env node
 const package = require('../libs/package')
 const program = require('commander')
 const didYouMean = require('didyoumean')
-
 // Setting edit distance to 60% of the input string's length
 didYouMean.threshold = 0.6
-
 program
 .command('package')
 .action(
   function () {
+    console.log('calling package command')
     package()
   }
 )
--- libs/package.js
+++ libs/package.js
@@ -1,5 +1,51 @@
+const fs = require('fs')
+const bach = require('bach')
+const recursive = require('recursive-readdir')
+const path = require('path')
+const CombinedStream = require('combined-stream2')
+const stream = require('stream')
+
+const targetMainSrc = './src/main/js'
+const targetDestFolder = './target/main/js'
+
 function package () {
-  console.log(("Packaging"))
+  let fileContents
+  process.cwd()
+  console.log('scanning the directories')
+  if (fs.existsSync(targetMainSrc)) {
+    let combinedStream = CombinedStream.create()
+    recursive(targetMainSrc, function (err, files) {
+      // `files` is an array of file paths
+      console.log(files)
+      for (const file of files) {
+        let passThrough = new stream.PassThrough()
+        combinedStream.append(passThrough)
+        passThrough.write("\n---------\n")
+        passThrough.end()
+        combinedStream.append(fs.createReadStream(file))
+      }
+      if (!fs.existsSync(targetDestFolder)) {
+        fs.mkdirSync(targetDestFolder, {
+          recursive: true
+        })
+      }
+      combinedStream.pipe(fs.createWriteStream(getFilePath().libFile))
+    })
+  }
+}
+
+function getConfig () {
+  let pomData = fs.readFileSync('xpom.json', 'utf8')
+  let config = JSON.parse(pomData)
+  return config
+}
+
+function getFilePath () {
+  let config = getConfig()
+  return {
+    libFile: targetDestFolder + config.package + '-' + config.version + '.lib',
+    metaFile: config.package + '-' + config.version + '.meta'
+  }
 }
 
 module.exports = package
