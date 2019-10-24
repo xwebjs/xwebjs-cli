49e20993ca59f530e7b7c1e4fcc558b5e4d7fecf
nick <nick@163.com>
2019-10-24 23:43:13 +0800

    Add command package feature

--- src/main/js/bin/command.js
+++ src/main/js/bin/command.js
@@ -1,14 +1,24 @@
-const package = require('../libs/package')
+const pack = require('../features/pack')
+const publish = require('../features/publish')
 const program = require('commander')
 const didYouMean = require('didyoumean')
 // Setting edit distance to 60% of the input string's length
 didYouMean.threshold = 0.6
+
 program
 .command('package')
 .action(
   function () {
     console.log('calling package command')
-    package()
+    pack()
+  }
+)
+program
+.command('publish')
+.action(
+  function () {
+    console.log('calling publish command')
+    publish()
   }
 )
 program.parse(process.argv)
