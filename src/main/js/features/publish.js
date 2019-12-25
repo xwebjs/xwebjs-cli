0f0ead4239886ccd04d5731b9173603195449316
nick <nick@163.com>
2019-12-25 17:44:16 +0800

    improvement

--- src/main/js/features/publish.js
+++ src/main/js/features/publish.js
@@ -3,12 +3,12 @@
 var request = require('request')
 var fs = require('fs')
 
-function publish() {
+function publish () {
   var r = request.post('http://localhost:3000/')
 // See http://nodejs.org/api/stream.html#stream_new_stream_readable_options
 // for more information about the highWaterMark
 // Basically, this will make the stream emit smaller chunks of data (ie. more precise upload state)
-  var upload = fs.createReadStream('target/main/js/x-1.0.xlib', { highWaterMark: 500 })
+  var upload = fs.createReadStream('target/main/js/x-1.0.xlib')
   upload.pipe(r)
   var upload_progress = 0
   upload.on('data', function (chunk) {
